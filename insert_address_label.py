e45d85781d26408991581211b0d4775ea469d8cd
Edmond Chuc <edmond@kurrawong.ai>
2023-06-19 13:43:45 +1000

    Working insert_address_label.py script

--- insert_address_label.py
+++ insert_address_label.py
@@ -1,10 +1,51 @@
+"""
+This script pulls the addresses from GraphDB and generates the compounded name label
+for each address and writes it to an n-quads file.
+
+Running this script takes just over 7 hours to complete.
+
+Loading the data into GraphDB via server files takes around 5 minutes.
+"""
+
+import os
+import shutil
 import time
 import logging
+from pathlib import Path
 
-from jinja2 import Template
+from rich.progress import track
+from rdflib import Graph, URIRef, Literal
+from rdflib.namespace import RDFS
+from pyoxigraph import Store, serialize
 
-from cam.graphdb import sparql, sparql_update
+from cam.graphdb import sparql
 from cam.compound_naming import get_compound_name_object, template_address_one_liner
+from cam.graph import prefixes
+
+
+def create_graph(path: str):
+    p = Path(path)
+    if p.exists():
+        shutil.rmtree(p)
+    p.mkdir(parents=True)
+
+    graph = Graph(store="Oxigraph", identifier="urn:graph:address-labels")
+    graph.open(path)
+
+    for key, val in prefixes.items():
+        graph.bind(key, val)
+
+    return graph
+
+
+def to_file(table_name: str, graph: Graph):
+    output_dir = Path("output")
+    output_dir.mkdir(exist_ok=True)
+    filename = Path(table_name + "-" + str(os.getpid()) + ".nq")
+
+    store: Store = graph.store._inner
+    quads = store.quads_for_pattern(None, None, None)
+    serialize(quads, str(output_dir / filename), "application/n-quads")
 
 
 def main() -> None:
@@ -14,43 +55,29 @@ def main() -> None:
     query = """
         SELECT ?iri
         WHERE {
-            VALUES ?iri {
-                <https://linked.data.gov.au/dataset/qld-addr/addr-1066374>
-                <https://linked.data.gov.au/dataset/qld-addr/addr-2285769>
-                <https://linked.data.gov.au/dataset/qld-addr/addr-1075435>
-                <https://linked.data.gov.au/dataset/qld-addr/addr-2522774>
-                <https://linked.data.gov.au/dataset/qld-addr/addr-1724075>
-                <https://linked.data.gov.au/dataset/qld-addr/addr-2819296>
-                <https://linked.data.gov.au/dataset/qld-addr/addr-82599>
-                <https://linked.data.gov.au/dataset/qld-addr/addr-2367581>
-                <https://linked.data.gov.au/dataset/qld-addr/addr-347127>
-            }
+            ?iri a <https://w3id.org/profile/anz-address/Address> .
         }
     """
 
     results = sparql(graphdb_url, repository_id, query)
     iris = [row["iri"]["value"] for row in results["results"]["bindings"]]
 
-    for iri in iris:
+    oxigraph_path = Path(f"oxigraph_data/address_labels")
+    graph = create_graph(str(oxigraph_path))
+
+    for iri in track(iris, description="Creating address labels..."):
         compound_name_object = get_compound_name_object(
             iri, f"{graphdb_url}/repositories/{repository_id}"
         )
         address = template_address_one_liner(compound_name_object)
 
         if address != "":
-            update_query = Template(
-                """
-                PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-                INSERT DATA {
-                    GRAPH <urn:address:labels> {
-                        <{{ iri }}> rdfs:label "{{ address }}" .
-                    }
-                }
-            """
-            ).render(iri=iri, address=address)
-            sparql_update(graphdb_url, repository_id, update_query)
+            graph.add((URIRef(iri), RDFS.label, Literal(address)))
         else:
             logging.warning(f"Could not template address for {iri}.")
+
+    print("Writing to disk...")
+    to_file("address_labels", graph)
 
 
 if __name__ == "__main__":
