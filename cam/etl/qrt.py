572ccfa239825d5aa4c5af945a623ec425dd5297
Edmond Chuc <edmond@kurrawong.ai>
2026-05-06 20:30:13 +1000

    refactor(qrt): update road IRI generation to use UUIDs

--- cam/etl/qrt.py
+++ cam/etl/qrt.py
@@ -1,9 +1,23 @@
+import uuid
+
 from rdflib import URIRef
 
 
+road_object_namespace = uuid.uuid5(
+    uuid.NAMESPACE_URL, "https://linked.data.gov.au/dataset/qld-addr/road/"
+)
+road_name_namespace = uuid.uuid5(
+    uuid.NAMESPACE_URL, "https://linked.data.gov.au/dataset/qld-addr/road-name/"
+)
+
+
 def get_road_object_iri(road_id: str):
-    return URIRef(f"https://linked.data.gov.au/dataset/qld-addr/road/{road_id}")
+    road_uuid = uuid.uuid5(road_object_namespace, str(road_id))
+    return URIRef(f"https://linked.data.gov.au/dataset/qld-addr/road/{road_uuid}")
 
 
 def get_road_name_iri(road_id: str):
-    return URIRef(f"https://linked.data.gov.au/dataset/qld-addr/road-name/{road_id}")
+    road_name_uuid = uuid.uuid5(road_name_namespace, str(road_id))
+    return URIRef(
+        f"https://linked.data.gov.au/dataset/qld-addr/road-name/{road_name_uuid}"
+    )
